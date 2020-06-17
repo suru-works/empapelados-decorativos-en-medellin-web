2280f51ed597c7763dddd3076c323397d5644ef1
DanielSBN <dansanchezbui@unal.edu.co>
2020-06-17 14:34:37 -0500

    Commit Inicial Frontend

--- edm-web/src/App.js
+++ edm-web/src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
-import Main from './components/MainComponent';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ConfigureStore } from './redux/configureStore';
 
-const store = ConfigureStore();
-
-class App extends Component {
-
-
-  render() {
-    return (
-      <Provider store={store}>
-        <BrowserRouter>
-          <div>
-            <Main />
-          </div>
-        </BrowserRouter>
-      </Provider>
-
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
