3adb9da5c7f465726ac81186cd15c5c29d6aca8e
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-06-30 11:45:04 -0500

    enviromental papu

--- edm-server/app.js
+++ edm-server/app.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Generate authentication key
-config.generateKey();
+//config.generateKey();
 
 // Passport initialization
 app.use(passport.initialize());
