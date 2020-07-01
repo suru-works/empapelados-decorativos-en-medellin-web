58bf16224e552b2c19b01ad7fe0d0a9aad183e37
DanielSBN <dansanchezbui@unal.edu.co>
2020-07-01 11:43:45 -0500

    CORS Fix

--- edm-server/routes/cors.js
+++ edm-server/routes/cors.js
@@ -2,7 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://www.empapeladosdecorativosenmedellin.com:3000/'];
+const whitelist = [
+    'http://www.empapeladosdecorativosenmedellin.com:3000/',
+    'https://www.empapeladosdecorativosenmedellin.com/'
+];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
