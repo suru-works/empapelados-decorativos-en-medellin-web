99100bac50c8994a743812f94d36f3ebaf22cf88
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-07-01 16:57:47 -0500

    backend full https

--- edm-server/app.js
+++ edm-server/app.js
@@ -32,13 +32,15 @@ connect.then((db) => {
 var app = express();
 
 // HTTPS
-/*app.all('*', (req, res, next) => {
+app.all('*', (req, res, next) => {
   if (req.secure) {
     return next();
   } else {
+    console.log('https://' + req.hostname + ':' + app.get('secPort') + req.url);
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+    
   }
-});*/
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
