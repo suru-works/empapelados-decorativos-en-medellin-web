3fb2ea5d536f63149d019f77b6dc7ef8d97684fe
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-07-01 09:29:17 -0500

    Galeria responsive vital
    No se si haga falta poner un scrollview pero weno, por ahora funciona con 3 productos, falta ver que pasa cuando hayan mas

--- edm-web/src/components/GalleryComponent.js
+++ edm-web/src/components/GalleryComponent.js
@@ -3,26 +3,23 @@ import { Card, CardImg, CardBody, CardTitle, CardText, CardImgOverlay } from 're
 import { Loading } from './LoadingComponent';
 import { baseFrontUrl } from '../shared/baseUrl';
 
-
-
-
 function RenderGallery ({product}) {
     return (
-        <Card bg= "secondary"
-        text= "secondary">
-            
 
+        //si quiere que sea cada carta de un ancho especifico ponerle de estilo a la card   style={{ width: '22rem' }}  , pero no es adaptativo
+        //mas que todo lo uso para hacer pruebas de que si salga X cards en tal pantalla
+
+        <Card>
             <CardBody>
                 <CardTitle>{product.name}</CardTitle>
 
-
                 <CardImg width="100%" src={baseFrontUrl + product.imageUrl} alt={product.name} />
-
 
                 <CardText>  {product.description}  </CardText>
                 <CardText>  Precio: {product.price}  </CardText>
                 <CardText>  Unidades: {product.units}  </CardText>
                 <CardText>  CÓMPRALO YA ANTES DE QUE HAYA INFLACIÓN  </CardText>
+                <CardText>  "{product.comments}"</CardText>
             </CardBody>
         </Card>
     );
@@ -32,7 +29,7 @@ const Gallery = (props) => {
 
     const gallery = props.products.map((product) => {
         return (
-            <div className="col-12 col-md-5 m-1"  key={product.id}>
+            <div className="col-12 col-lg-3 col-md-4 col-sm-6"  key={product.id}>
                 <RenderGallery product={product}/>
             </div>
         );
@@ -63,16 +60,16 @@ const Gallery = (props) => {
     return (
         <div className="container">
             <div className="row">
-                
                 <div className="col-12">
-                    <h3>Galeria chaval</h3>
+                    <h3>Galeria que se adapta a tu pantalla</h3>
                     <hr />
                 </div>                
             </div>
-            <div className="row">
+            <div className="row col-12">
                 {gallery}
             </div>
         </div>
+
     );
 }
 
