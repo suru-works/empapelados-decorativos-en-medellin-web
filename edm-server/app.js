9df5d6fb3a8e3d25621297f72cfac9b7eef10e13
DanielSBN <dansanchezbui@unal.edu.co>
2020-07-01 12:03:14 -0500

    Backend HTTPS

--- edm-server/app.js
+++ edm-server/app.js
@@ -31,6 +31,15 @@ connect.then((db) => {
 // Express initialization
 var app = express();
 
+// HTTPS
+/*app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  } else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+  }
+});*/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
