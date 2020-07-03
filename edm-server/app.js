cab6aa0313a31159f28088e335aa717444c8513a
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-07-03 13:55:36 -0500

    somos idiotas server

--- edm-server/app.js
+++ edm-server/app.js
@@ -48,7 +48,7 @@ app.all('*', (req, res, next) => {
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Host');
 
   //intercepts OPTIONS method
   if ('OPTIONS' === req.method) {
