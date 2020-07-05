f24e0ae4c8676dc2f01075c9516c06f481a6d012
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-07-04 23:49:42 -0500

    Intentando ponerle EventListeners a cada carta
    para ya no va a haber que usar onclick que esa wea es desconfianzuda y asi de pronto poder pasar parametros

--- edm-web/src/components/GalleryComponent.js
+++ edm-web/src/components/GalleryComponent.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Card, CardImg, CardBody, CardTitle, CardText, CardImgOverlay } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardTitle, CardText, CardImgOverlay, Modal, ModalHeader, ModalBody  } from 'reactstrap';
 import { Loading } from './LoadingComponent';
 import { baseFrontUrl } from '../shared/baseUrl';
 
-function RenderGallery ({product}) {
+function RenderGallery ({product, toggleLoginModal}) {
+
     return (
 
         //si quiere que sea cada carta de un ancho especifico ponerle de estilo a la card   style={{ width: '22rem' }}  , pero no es adaptativo
         //mas que todo lo uso para hacer pruebas de que si salga X cards en tal pantalla
 
-        <Card>
+        <Card onClick={toggleLoginModal}>
             <CardBody>
                 <CardTitle>{product.name}</CardTitle>
 
@@ -25,32 +26,84 @@ function RenderGallery ({product}) {
     );
 }
 
-const Gallery = (props) => {
-
-    const gallery = props.products.map((product) => {
-        return (
-            <div className="col-12 col-lg-3 col-md-4 col-sm-6"  key={product.id}>
-                <RenderGallery product={product}/>
-            </div>
-        );
-    });
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <h3>Galeria que se adapta a tu pantalla</h3>
-                    <hr />
-                </div>                
-            </div>
-            <div className="row col-12">
-                {gallery}
-            </div>
-        </div>
-
-    );
-
+function RenderModal ({isLoginModalOpen, toggleLoginModal, imagen}) {
     
+    return (
+
+        <Modal isOpen={isLoginModalOpen} toggle={toggleLoginModal}>
+                    <ModalHeader toggle ={toggleLoginModal}>Ingresar</ModalHeader>
+
+                    <ModalBody>
+
+                    <CardImg
+                            className="col-3"
+                            src={imagen}
+                        />
+                    
+                    </ModalBody>
+                    </Modal>
+    );
+}
+
+class Gallery extends Component{
+
+
+    constructor(props) {
+        
+        super(props);
+        this.state = {
+            isLoginModalOpen: false,
+            selectedProduct: "https://minas.medellin.unal.edu.co/images/minas/equipo/directores-departamento/jndVelas.jpg"
+        };
+
+        this.toggleLoginModal = this.toggleLoginModal.bind(this);
+    }
+
+    toggleLoginModal() {
+        this.setState({
+            isLoginModalOpen: !this.state.isLoginModalOpen
+        });
+    }
+
+
+    render(){
+
+        const galeria = this.props.products.map((product) => {
+            try {
+                return (
+                    <div className="col-12 col-lg-3 col-md-4 col-sm-6" id={product._id} key={product._id} >
+                        <RenderGallery product={product} toggleLoginModal={this.toggleLoginModal} />
+                    </div>
+                );
+            }
+            catch(err) {
+                console.log("No hizo el id ese")
+            }    
+            
+            
+        });
+
+        return (
+            <div className="container">
+             
+                <div className="row">
+                    <div className="col-12">
+                        <h3>Galeria que se adapta a tu pantalla</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row col-12">
+                    {galeria}
+
+                    
+                </div>
+                <RenderModal isLoginModalOpen={this.state.isLoginModalOpen} toggleLoginModal={this.toggleLoginModal} imagen={"https://minas.medellin.unal.edu.co/images/minas/equipo/directores-departamento/jndVelas.jpg"}/>
+
+            </div>
+    
+        )
+    }
+  
 }
 
 export default Gallery;   
