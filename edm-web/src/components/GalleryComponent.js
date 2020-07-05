89ec7399caa01be054c6058319b3356f2984d8db
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-07-05 15:54:14 -0500

    migrar gallery

--- edm-web/src/components/GalleryComponent.js
+++ edm-web/src/components/GalleryComponent.js
@@ -3,27 +3,6 @@ import { Card, CardImg, CardBody, CardTitle, CardText, CardImgOverlay, Modal, Mo
 import { Loading } from './LoadingComponent';
 import { baseFrontUrl } from '../shared/baseUrl';
 
-function RenderGallery(props) {
-
-    return (
-
-        //si quiere que sea cada carta de un ancho especifico ponerle de estilo a la card   style={{ width: '22rem' }}  , pero no es adaptativo
-        //mas que todo lo uso para hacer pruebas de que si salga X cards en tal pantalla
-
-        <Card onClick={props.onClick}>
-            <CardBody>
-                <CardTitle>{props.item.name}</CardTitle>
-
-                <CardImg width="100%" src={baseFrontUrl + props.item.imageUrl} alt={props.item.name} />
-
-                <CardText>  {props.item.description}  </CardText>
-                <CardText>  Precio: {props.item.price}  </CardText>
-                <CardText>  Unidades: {props.item.units}  </CardText>
-
-            </CardBody>
-        </Card>
-    );
-}
 
 class Gallery extends Component {
 
@@ -33,14 +12,21 @@ class Gallery extends Component {
         super(props);
         this.state = {
             isModalOpen: false,
-            selectedProduct: this.props.products[0]
+            selectedProduct: {}
         };
 
         this.toggleModal = this.toggleModal.bind(this);
         this.showProduct = this.showProduct.bind(this);
     }
 
+    componentWillMount(){
+        this.setState({
+            selectedProduct: this.props.products[0]
+        });
+    }
+
     showProduct(product) {
+        console.log(product);
         this.setState({
             selectedProduct: product
         });
@@ -58,9 +44,11 @@ class Gallery extends Component {
 
         const galeria = this.props.products.map((product) => {
             try {
+                
                 return (
                     <div className="col-12 col-lg-3 col-md-4 col-sm-6" key={product._id}>
-                        <Card onClick={(product) => this.showProduct(product)}>
+                        <Card onClick={product => {this.showProduct(product)}}>
+                        
                             <CardBody>
                                 <CardTitle>{product.name}</CardTitle>
 
