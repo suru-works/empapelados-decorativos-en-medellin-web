ef3a409bb2c8641cbffc26753e56a9c091a015ba
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-07-05 15:14:36 -0500

    F*ck go back

--- edm-server/app.js
+++ edm-server/app.js
@@ -48,8 +48,7 @@ app.all('*', (req, res, next) => {
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Set-Cookie');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Host');
 
   //intercepts OPTIONS method
   if ('OPTIONS' === req.method) {
