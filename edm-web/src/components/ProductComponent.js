0a5f8bd36126636d49e4ebdb7e9d3e4b032cb8a5
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-07-05 22:07:46 -0500

    Modal bien dividido y perrón
    Ahora ya se muestra en dos columnas y todo

--- edm-web/src/components/ProductComponent.js
+++ edm-web/src/components/ProductComponent.js
@@ -27,7 +27,7 @@ class Product extends Component {
 
     render() {
 
-        {console.log("Imprimeme un producto pls " + this.props.product)}
+        //console.log("Imprimeme un producto pls " + this.props.product)}
 
         return (
 
@@ -42,38 +42,37 @@ class Product extends Component {
 
                         <CardImg width="100%" src={baseFrontUrl + this.props.product.imageUrl} alt={this.props.product.name} />
 
-                        <CardText>  {this.props.product.description}  </CardText>
-                        <CardText>  Precio: {this.props.product.price}  </CardText>
-                        <CardText>  Unidades: {this.props.product.units}  </CardText>
-
                     </CardBody>
                 </Card>
 
-                {console.log("imprimo a ver si esto le puedo poner una key " + this.props.product._id)}
-
             
+                <Modal className="modal-lg" isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
 
-            
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-
-                    <ModalHeader toggle={this.toggleModal}>Ingresar</ModalHeader>
+                    <ModalHeader toggle={this.toggleModal}>{this.props.product.name}</ModalHeader>
 
                     <ModalBody>
-                        <div className="row">
-                            <div className="col-md-12 col-sm-12 col-xm-12 m-1">
 
-                                <Card key={this.props.product._id}>
-                                    <CardImg top src={baseFrontUrl + this.props.product.imageUrl} alt={this.props.product.name} />
+                        <div className="d-flex space-around">       
 
-                                    <CardBody>
-                                        <CardTitle>{this.props.product.name}</CardTitle>
-                                        <CardText>{this.props.product.description}</CardText>
-                                    </CardBody>
-                                </Card>
+                            <Card className=" mr-2" key={this.props.product._id}>
+                                <CardImg top src={baseFrontUrl + this.props.product.imageUrl} alt={this.props.product.name} />
+                            </Card>
 
-                            </div>
+                            <Card key={this.props.product._id}>
+                                
+                                <CardBody>
+                                    <CardTitle> Objeto Premium, futuro GOTY </CardTitle>
+                                    <CardText>  {this.props.product.description}  </CardText>
+                                    <CardText>  Precio: {this.props.product.price}  </CardText>
+                                    <CardText>  Unidades: {this.props.product.units}  </CardText>
+                                    <CardText>  Comentarios: {this.props.product.comments}  </CardText>
+                                    <CardText>      </CardText>
+                                    <CardText>  Mhhhh eres el primero en llegar. Para que no te sientas solo aqui esta una chika bailando  :V  </CardText>
+                                    <CardImg width="100%" src="https://media.tenor.com/images/fe3826b59f80f5e6c7cc04eb474fb44d/tenor.gif" alt="chika dance" />
+                                </CardBody>
+                            </Card>
 
-                        </div>
+                        </div> 
 
                     </ModalBody>
                 </Modal>
