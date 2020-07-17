50d021e7a0da9b78bd815c447052f2dba1913a25
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-07-16 20:38:54 -0500

    delete media

--- edm-server/routes/mediaRouter.js
+++ edm-server/routes/mediaRouter.js
@@ -5,6 +5,10 @@ const path = require("path");
 const fs = require("fs");
 const shortid = require('shortid');
 
+const { promisify } = require('util')
+
+const unlinkAsync = promisify(fs.unlink);
+
 const authenticate = require('../authenticate');
 
 const router = express.Router();
@@ -41,8 +45,49 @@ const subirArchivo = async (req,res,next) =>{
     })
 }
 
+const eliminarArchivo = async (req,res,next) => {
+
+    try {
+        fs.unlinkSync(process.env.MEDIA_URL+'images/products/'+req.params.fileId);        
+    } catch (error) {
+        //console.log(error);
+        //  res.json({error: error.code});
+        //res.statusCode = 500;
+        res.status(500).json({msg: 'Error al eliminar el archivo'});
+    }
+    
+}
+
+const actualizarArchivo = async (req,res,next) =>{
+    const upload = multer({
+        storage: fileStorage = multer.diskStorage({
+            destination: (req, file, cb) => {
+                //cb(null, __dirname + '/../uploads')
+                console.log(process.env.MEDIA_URL);
+                cb(null, process.env.MEDIA_URL+'images/products/')
+            },
+            filename: (req, file, cb) => {
+                const extension = file.originalname.substring(file.originalname.lastIndexOf('.'), file.originalname.length);
+                cb(null, `${shortid.generate()}${extension}`)
+            }
+        })
+    }).single('file');
+    upload(req, res, async (error) =>{
+        console.log(req.file);
+        res.json({archivo: req.file.filename});
+    })
+}
+
 
 router.post("/image", subirArchivo
+
+);
+
+router.put("/image/:fileId", actualizarArchivo
+
+);
+
+router.delete("/image/:fileId", eliminarArchivo
 
 );
 
