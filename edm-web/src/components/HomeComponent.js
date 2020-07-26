55772fafde0b874da664f76a0ba7b3bc47b1a2ce
Ismael Restrepo Vélez <irestrepov@unal.edu.co>
2020-07-25 21:30:46 -0500

    Homecomponent kawai, repositorio no cagado
    Nada, ahora muestra las imagenes que hay en products, se puede meter videos de youtube y todo, y tiene los pellets o como se llamen los puntitos.
    Eso si, el restringir el tamaño de una imagen se supone que se hace con la propiedad "sizes", pero no me ha funcionado de nada, puede que simplemente no está bien implementado y toque hacerlo por medio de divs

--- edm-web/src/components/HomeComponent.js
+++ edm-web/src/components/HomeComponent.js
@@ -8,25 +8,22 @@ import '../styles.css'; // Tell webpack that HomeComponent.js uses these styles
 
 function Home(props) {
 
-    const images = [
-        {
-          original: 'https://picsum.photos/id/1018/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1018/250/150/',
-        },
-        {
-          original: 'https://picsum.photos/id/1015/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1015/250/150/',
-        },
-        {
-          original: 'https://picsum.photos/id/1019/1000/600/',
-          thumbnail: 'https://picsum.photos/id/1019/250/150/',
-        },
-      ];
+
+
+      const images = props.products.map((item) => {
+        return(
+            {
+                original: baseFrontUrl + item.imageUrl,
+                thumbnail: baseFrontUrl + item.imageUrl
+              }
+            
+        );
+    });
 
     return(
         <div className="container">
             <div className="row align-items-center justify-content-center">
-                <ImageGallery items={images} />
+                <ImageGallery items={images} showBullets={true} autoPlay={true} slideDuration={600} />
             </div>
             <div className="row col-12 align-items-center justify-content-center">
                 <Link to='/galeria'>
